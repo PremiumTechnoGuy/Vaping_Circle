ebda9622f42e659b478c02040ceec4c69d32356d
Hanzla Saadi <hanzalasaadi@gmail.com>
2024-01-26 22:18:19 +0500

    rendering categories with respective filters

--- src/Sub_Component/Retail/SmallNavbar.js
+++ src/Sub_Component/Retail/SmallNavbar.js
@@ -1,10 +1,232 @@
 import React from "react";
 import "./smallNavbar.css";
 import { Outlet, Link } from "react-router-dom";
+import axios from "axios";
+import { apiUrl } from "../../data/env";
+
+function CategoryComponent({ allFilters, category }) {
+  const thisCategoryFilters = allFilters?.filter(
+    (fil) => fil.categoryId === category._id
+  );
+  console.log(category.name, thisCategoryFilters);
+  return (
+    <li role="menuitem">
+      <a
+        href
+        class="hover:underline  hover:font-bold hover:decoration-blue-800 hover:underline-offset-[19px] hover:text-blue-900"
+      >
+        {category.name}
+      </a>
+      <div
+        className="mega-menu1  left-[0px]"
+        id="submenu"
+        aria-hidden="true"
+        role="menu"
+      >
+        {thisCategoryFilters?.map((aFilter) => {
+          return (
+            <div className="nav-column">
+              <li role="menuitem">
+                <h3 class="font-bold hover:text-blue-900">{aFilter.name}</h3>
+                <div
+                  className="mega-menu  left-[0px]"
+                  aria-hidden="true"
+                  role="menu"
+                >
+                  <div className="nav-column ">
+                    <h3 className="fw-bold"></h3>
+                    <ul>
+                      {aFilter.options.map((option) => (
+                        <li role="menuitem">
+                          <a href>{option}</a>
+                        </li>
+                      ))}
+                      {/* <li role="menuitem">
+                        <a href="#">Starter Kits</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">Advanced Kits</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">Vape Pen Kits</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">Pod Kits</a>
+                      </li> */}
+                    </ul>
+                  </div>
+                  {/* <div className="nav-column">
+                    <h3 className="fw-bold"></h3>
+                    <ul>
+                      <li role="menuitem">
+                        <a href="#">Disposables Vapes</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">MLT Vape Kits</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">Bundle Kits</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">Shop All Vape Kits</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="nav-column">
+                    <h3 className="fw-bold">User Level</h3>
+                    <ul>
+                      <li role="menuitem">
+                        <a href="#">First Time Switcher</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">Intermediate / Mid Range Kits</a>
+                      </li>
+                      <li role="menuitem">
+                        <a href="#">Advanced User Vapes</a>
+                      </li>
+                    </ul>
+                  </div> */}
+                  <div className="flex col gap-2  float-right  p-3">
+                    <img
+                      style={{ height: 200 }}
+                      src="https://www.geekvape.com/upload/blog/blog-7.401.jpg"
+                      width="400px"
+                    />
+                  </div>
+                </div>
+              </li>
+            </div>
+          );
+        })}
+
+        {/* <div className="nav-column">
+          <li role="menuitem">
+            <h3 class="fw-bold hover:text-blue-900">Shop By Feature</h3>
+            <div
+              className="mega-menu  left-[0px]"
+              aria-hidden="true"
+              role="menu"
+            >
+              <div className="nav-column ">
+                <h3 className="fw-bold"></h3>
+                <ul>
+                  <li role="menuitem">
+                    <a href="#">Inhale- Activated Kits</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Rechargable Kits</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Closed Pod Kits</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Adjustable Airflow</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="flex col float-right  p-3">
+                <img
+                  style={{ height: 200 }}
+                  src="https://www.geekvape.com/upload/blog/blog-7.401.jpg"
+                  width="600px"
+                />
+              </div>
+            </div>
+          </li>
+        </div>
+
+        <div className="nav-column">
+          <li role="menuitem">
+            <h3 class="fw-bold hover:text-blue-900">Featured Brands</h3>
+            <div
+              className="mega-menu  left-[0px]"
+              aria-hidden="true"
+              role="menu"
+            >
+              <div className="nav-column ">
+                <h3 className="fw-bold"></h3>
+                <ul>
+                  <li role="menuitem">
+                    <a href="#">Aspire</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Elf Bar</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Geek Vape</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Innokin</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">JUUL</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="nav-column mb-3">
+                <h3 className="fw-bold"></h3>
+                <ul>
+                  <li role="menuitem">
+                    <a href="#">OXVA</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">SMOKE</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Vaporesso</a>
+                  </li>
+                  <li role="menuitem">
+                    <a href="#">Voopoo</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="flex col gap-2 float-right mb-2 p-3">
+                <img
+                  width="300px"
+                  style={{ height: 200 }}
+                  src="https://ik.imagekit.io/p2slevyg1/images.jpg?updatedAt=1704137315344"
+                />
+                <img
+                  style={{ height: 200 }}
+                  src="https://ik.imagekit.io/p2slevyg1/geekvape-z200-kit_500x-PhotoRoom.jpg?updatedAt=1704137095886"
+                  width="450px"
+                />
+              </div>
+            </div>
+          </li>
+        </div> */}
+      </div>
+    </li>
+  );
+}
 
 function SmallNavbar() {
+  const [categories, setCategories] = React.useState([]);
+  const [filters, setFilters] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(`${apiUrl}/api/v1/category?sort=priority`)
+      .then((res) => setCategories(res.data.data))
+      .catch((err) => console.error(err));
+
+    axios
+      .get(`${apiUrl}/api/v1/filter?sort=priority`)
+      .then((res) => setFilters(res.data.data))
+      .catch((err) => console.error(err));
+
+    console.log("cats", categories, "filters", filters);
+  }, []);
+
   return (
-    <div id="smallnav" class="absolute z-5  hidden md:block" style={{zIndex:'5'}}>
+    <div
+      id="smallnav"
+      class="absolute z-5  hidden md:block"
+      style={{ zIndex: "5" }}
+    >
       <div
         className="menu-wrapper "
         class="pb-2 bg-white"
@@ -12,7 +234,14 @@ function SmallNavbar() {
         style={{ borderBottom: "1px solid #00000014" }}
       >
         <ul className="nav gap-3 w-full" role="menubar">
-          <li role="menuitem">
+          {categories?.map((category) => (
+            <CategoryComponent
+              key={category._id}
+              allFilters={filters}
+              category={category}
+            />
+          ))}
+          {/* <li role="menuitem">
             <a
               href="#"
               class="hover:underline  hover:font-bold hover:decoration-blue-800 hover:underline-offset-[19px] hover:text-blue-900"
@@ -261,7 +490,7 @@ function SmallNavbar() {
             >
               CBD
             </a>
-          </li>
+          </li> */}
           <div
             style={{
               display: "flex",
@@ -279,7 +508,6 @@ function SmallNavbar() {
               </button>
             </Link>
           </div>
-          
         </ul>
       </div>
       <Outlet />
