b0ccbf794de8e31173065d7af44bb2277cb9c821
Hanzla Saadi <hanzalasaadi@gmail.com>
2024-01-31 21:34:55 +0500

    view all categories page

--- src/Sub_Component/Retail/Category/CategoryPage.js
+++ src/Sub_Component/Retail/Category/CategoryPage.js
@@ -6,10 +6,10 @@ import { data } from "./../../../data/Cdata.js";
 import { Link, Outlet } from "react-router-dom";
 import Fixed_Component from "../Fixed_Component.js";
 
-function CategoryPage() {
+function CategoryPage({ categories }) {
   return (
     <div class="mt-36 md:mt-64 ">
-       <Fixed_Component />
+      <Fixed_Component />
       <h1 class="fs-1 font-bold mb-5 text-center">Categories</h1>
 
       <Container
@@ -17,7 +17,7 @@ function CategoryPage() {
         className="d-flex justify-content-center mb-5 align-items-center"
       >
         <Row xs={2} md={3}>
-          {data.map((item) => (
+          {categories?.map((item, i) => (
             <Col
               key={item.id}
               class="flex justify-center items-center"
@@ -28,20 +28,22 @@ function CategoryPage() {
               }}
             >
               <div id="content" class="">
-                <div className="item e6">
-                  <img
-                    class="relative object-cover transition ease-in-out delay-75  hover:-translate-y-1 hover:scale-105 duration-150"
-                    src={item.image}
-                    alt={item.name}
-                    width="330px"
-                    height="270px"
-                  />
-                  <button
-                    class=" p-3 left-[2rem] lg:left-[4rem] right-[2rem]  md:w-44 absolute shadow-md bg-white font-bold text-[14px] md:fs-6 rounded-md text-[#59A0B8] bottom-[2rem] md:bottom-[3rem] z-5"
-                  >
-                    {item.name}
-                  </button>
-                </div>
+                <Link
+                  to={`/productPage/${categories[i]?._id}/${categories[i]?.name}`}
+                >
+                  <div className="item e6">
+                    <img
+                      class="relative object-cover transition ease-in-out delay-75  hover:-translate-y-1 hover:scale-105 duration-150"
+                      src={item.image}
+                      alt={item.name}
+                      width="330px"
+                      height="270px"
+                    />
+                    <button class=" p-3 left-[2rem] lg:left-[4rem] right-[2rem]  md:w-44 absolute shadow-md bg-white font-bold text-[14px] md:fs-6 rounded-md text-[#59A0B8] bottom-[2rem] md:bottom-[3rem] z-5">
+                      {item.name}
+                    </button>
+                  </div>
+                </Link>
               </div>
             </Col>
           ))}
