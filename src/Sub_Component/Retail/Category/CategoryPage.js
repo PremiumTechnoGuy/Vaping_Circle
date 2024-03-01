93cb2147be105d8e234fba0459f4f9731bd2cb2d
Hanzla Saadi <hanzalasaadi@gmail.com>
2024-03-02 02:59:38 +0500

    bug fix category page

--- src/Sub_Component/Retail/Category/CategoryPage.js
+++ src/Sub_Component/Retail/Category/CategoryPage.js
@@ -17,46 +17,50 @@ function CategoryPage({ categories, filters }) {
         className="d-flex justify-content-center mb-5 align-items-center"
       >
         <Row xs={2} md={3}>
-          {categories?.map((item, i) => (
-            <Col
-              key={item.id}
-              class="flex justify-center items-center"
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <div id="content" class="">
-                <Link
-                  to={`/productPage/${categories[i]?._id}/${categories[i]?.name}`}
-                >
-                  <div className="item e6">
-                    <img
-                      class="relative object-cover transition ease-in-out delay-75  hover:-translate-y-1 hover:scale-105 duration-150"
-                      src={
-                        item.image.url
-                          ? item.image.url.replace(
-                              "/category",
-                              "/tr:ar-1-1,w-285.5/category"
-                            )
-                          : item.image.replace(
-                              "/category",
-                              "/tr:ar-1-1,w-285.5/category"
-                            ) || ""
-                      }
-                      alt={item.name}
-                      width="330px"
-                      height="270px"
-                    />
-                    <button class=" p-3 left-[2rem] lg:left-[4rem] right-[2rem]  md:w-44 absolute shadow-md bg-white font-bold text-[14px] md:fs-6 rounded-md text-[#59A0B8] bottom-[2rem] md:bottom-[3rem] z-5">
-                      {item.name}
-                    </button>
-                  </div>
-                </Link>
-              </div>
-            </Col>
-          ))}
+          {categories?.map((item, i) => {
+            let url = "";
+            if (item.image.url)
+              url = item.image.url.replace(
+                "/category",
+                "/tr:ar-1-1,w-285.5/category"
+              );
+            else if (item.image)
+              url = item.image.replace(
+                "/category",
+                "/tr:ar-1-1,w-285.5/category"
+              );
+            else url = "";
+            return (
+              <Col
+                key={item.id}
+                class="flex justify-center items-center"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div id="content" class="">
+                  <Link
+                    to={`/productPage/${categories[i]?._id}/${categories[i]?.name}`}
+                  >
+                    <div className="item e6">
+                      <img
+                        class="relative object-cover transition ease-in-out delay-75  hover:-translate-y-1 hover:scale-105 duration-150"
+                        src={url}
+                        alt={item.name}
+                        width="330px"
+                        height="270px"
+                      />
+                      <button class=" p-3 left-[2rem] lg:left-[4rem] right-[2rem]  md:w-44 absolute shadow-md bg-white font-bold text-[14px] md:fs-6 rounded-md text-[#59A0B8] bottom-[2rem] md:bottom-[3rem] z-5">
+                        {item.name}
+                      </button>
+                    </div>
+                  </Link>
+                </div>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
 
