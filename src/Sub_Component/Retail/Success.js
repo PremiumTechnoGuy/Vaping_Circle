4f9372c72e62b55f28f1c7e86bcb24dc2ca8d278
HanzlaLatitude <hanzalasaadi@gmail.com>
2024-03-10 07:08:40 +0500

    handle successful payment

--- src/Sub_Component/Retail/Success.js
+++ src/Sub_Component/Retail/Success.js
@@ -4,16 +4,28 @@ import axios from "axios";
 import { apiUrl } from "../../data/env";
 import Loader from "../Loader/loader";
 import toast, { Toaster } from "react-hot-toast";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function Success() {
   const [isLoading, setIsLoading] = React.useState(true);
 
+  const nav = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const auth = useAuth();
 
+  const token = localStorage.getItem("token");
+
   React.useEffect(() => {
-    const token = localStorage.getItem("token");
+    const tId = searchParams.get("t");
+    const orderId = searchParams.get("s");
+    const eventId = Number(searchParams.get("eventId"));
+    const eci = searchParams.get("eci");
+    let orderDocId = "";
+
     if (token) {
-      const id = toast.loading("Verifying...");
+      // const id = toast.loading("Verifying...");
       const config = {
         headers: { Authorization: `Bearer ${token}` },
       };
@@ -22,22 +34,75 @@ function Success() {
         .then((res) => {
           console.log(res.data);
           auth.login(token, res.data.data);
-          toast.success("Verified Succesfully!", { id });
-          setTimeout(() => {
-            setIsLoading(false);
-            console.log(res.data.data);
-          }, 1500);
+          // toast.success("Fetched Data!");
+
+          if (!tId) {
+            toast.error("error verifying payment (id not present)!");
+            setTimeout(() => {
+              nav("/myOrder");
+            }, 1000);
+          }
+
+          if (!orderId) {
+            toast.error("error verifying payment (order id not present)!");
+            setTimeout(() => {
+              nav("/myOrder");
+            }, 1000);
+          } else {
+            const orderObj = res.data.data.orders.find(
+              (oObj) => oObj.vivaOrderId === orderId
+            );
+            if (!orderObj) {
+              toast.error("Could not find the order in user's DB!");
+              toast.error("Email us to sort your order!", { duration: 5000 });
+              setTimeout(() => {
+                nav("/");
+              }, 5000);
+            } else {
+              orderDocId = orderObj.orderId;
+            }
+          }
+
+          if (eventId !== 0) toast.error("error occured in transaction");
+
+          handleUpdateOrder(orderDocId, orderId, tId, eci, eventId);
         })
         .catch((err) => {
           setIsLoading(false);
           console.log(err);
-          toast.error("Could not verify!", { id });
+          toast.error("Could not verify user!");
         });
     } else {
       toast.error("Could not verify login!");
       setIsLoading(false);
     }
   }, []);
+
+  const handleUpdateOrder = (orderDocId, oId, tId, eci, eventId) => {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    axios
+      .patch(
+        `${apiUrl}/api/v1/order/${orderDocId}`,
+        { status: "paid", vivaTransactionCode: tId, eci, eventId },
+        config
+      )
+      .then((res) => {
+        toast.success("Payment & Verification Successful");
+        setTimeout(() => {
+          nav("/myOrder");
+        }, 2500);
+      })
+      .catch((err) => {
+        toast.error(
+          err.response?.data?.message || "Couldn't update DB with new Payment!"
+        );
+        setTimeout(() => {
+          nav("/myOrder");
+        }, 3500);
+      });
+  };
 
   return (
     <>
