f026ffeb1faeab471d431bd653a9316e251171d6
Your Name <you@example.com>
2024-03-12 19:40:24 +0500

    order page styling

--- src/Sub_Component/Retail/MyOrder.js
+++ src/Sub_Component/Retail/MyOrder.js
@@ -49,28 +49,23 @@ function MyOrder({ categories, filters }) {
       <Fixed_Component categories={categories} filters={filters} />
       <h1 class="fs-2 font-bold py-5 text-center">My Orders</h1>
       <Container>
-        <hr class="mx-4 mb-2" />
+        <hr class="mx-4 mb-2 " />
         {allOrders?.map((order) => {
           return (
             <>
-              <div className="md:d-flex">
-                <h2 class="fs-6 mb-2 font-bold py-2 pl-5 text-left d-flex gap-2">
+              <div className="md:d-flex mt-3">
+                {/* <h2 class="fs-6 mb-2 font-bold py-2 pl-5 text-left d-flex gap-2">
                   <span>
                     <MdFormatListNumbered />
                   </span>
                   <span>
-                    Order No:{" "}
-                    {`${new Date(order.createdAt).getTime()}`.slice(5)}
+                    Order No:
+                    <span className="text-[#59A0B8]"> {`${new Date(order.createdAt).getTime()}`.slice(5)}</span>
                   </span>
-                </h2>
-                <h2 class="fs-6 mb-2 font-bold py-2 pl-5 text-left d-flex gap-2">
-                  <span>
-                    <TbTruckDelivery />
-                  </span>{" "}
-                  <span>Status: '{order.status}'</span>
-                </h2>
+                </h2> */}
+
                 {order.status === "cancelled" ? null : (
-                  <h2 class="fs-6 mb-2 font-bold py-2 pl-5 text-left d-flex gap-2">
+                  <h2 class="fs-6 mb-2 font-bold py-2 pl-5 text-left  d-flex gap-2">
                     <span>
                       <MdOutlinePinDrop />
                     </span>{" "}
@@ -83,91 +78,106 @@ function MyOrder({ categories, filters }) {
                   </h2>
                 )}
               </div>
-              <Row class="shadow-md mb-3">
-                {order.commodities.map((commodity) => {
-                  return (
-                    <Col
-                      key={commodity.uId}
-                      class="flex justify-start items-start"
-                      style={{
-                        display: "flex",
-                        justifyContent: "start",
-                        alignItems: "start",
-                      }}
-                    >
-                      {" "}
-                      <span>
-                        <img
-                          src={
-                            commodity.image ||
-                            "https://ik.imagekit.io/ql8u22y4o/products/product-65e4724a2d2972e0b9a855a0-1709470282925.jpeg"
-                          }
-                          alt={commodity.nm}
-                          style={{ maxWidth: "none" }}
-                          class="p-1 xs:m-0 md:px-12 md:py-3 w-[7rem]  md:w-[16rem] "
-                        />{" "}
-                      </span>
-                      <span>
-                        <div
+              <Row class="shadow-md mb-3 ">
+                <ol type="1">
+                  <li>
+                    {order.commodities.map((commodity) => {
+                      return (
+                        <Col
+                          key={commodity.uId}
+                          class="d-flex justify-start items-start bg-light COL-1 mt-2 mb-2"
                           style={{
                             display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "space-evenly",
+                            justifyContent: "start",
+                            alignItems: "start",
                           }}
                         >
-                          <div class="px-2 py-3 mb-5">
-                            <p class="text-[#000000] font-bold text-sm pb-1">
-                              {commodity.nm}
-                            </p>
+                          {" "}
+                          <span>
+                            <img
+                              src={
+                                commodity.image ||
+                                "https://ik.imagekit.io/ql8u22y4o/products/product-65e4724a2d2972e0b9a855a0-1709470282925.jpeg"
+                              }
+                              alt={commodity.nm}
+                              style={{ maxWidth: "none" }}
+                              class="p-1 xs:m-0 md:px-12 md:py-3 w-[7rem] md:w-[10rem] "
+                            />{" "}
+                          </span>
+                          <span>
+                            <h2 class="fs-6  font-bold py-1 pl-2 text-left d-flex gap-2">
 
-                            {commodity.variants?.map((vr) => (
-                              <p class="pb-2">
-                                {vr.variantType}: {vr.chosenOption.optionValue}
-                              </p>
-                            ))}
-                            <div class="flex justify-between items-center block md:hidden">
                               <span>
-                                <p class=" py-3 md:hidden block  text-[#59A0B8] md:pl-[6rem] md:pr-[3rem] text-[#000000] font-bold text-sm pr-5">
-                                  £
-                                  {(
-                                    commodity.price * commodity.quantity
-                                  ).toFixed(2)}
-                                </p>
+                                Order No:
+                                <span className="text-[#59A0B8]"> {`${new Date(order.createdAt).getTime()}`.slice(5)}</span>
                               </span>
+                              <span className="ml-20">
+                                <TbTruckDelivery />
+                              </span>{" "}
+                              <span>Status: <span className="text-[#59A0B8]"> '{order.status}'</span></span>
+                            </h2>
+
+                            <div
+                              style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "space-evenly",
+                              }}
+                            >
+                              <div class="px-2 py-3 mb-5">
+                                <p class="text-[#000000] font-bold text-sm pb-1">
+                                  {commodity.nm}
+                                </p>
+
+                                {commodity.variants?.map((vr) => (
+                                  <p class="pb-2 font-bold ">
+                                    {vr.variantType}:<span className="text-[#59A0B8]"> {vr.chosenOption.optionValue}</span>
+                                  </p>
+                                ))}
+                                <div class="flex justify-between items-center block md:hidden">
+                                  <span>
+                                    <p class=" py-3 md:hidden block  text-[#59A0B8] md:pl-[6rem] md:pr-[3rem] text-[#000000] font-bold text-sm pr-5">
+                                      £
+                                      {(
+                                        commodity.price * commodity.quantity
+                                      ).toFixed(2)}
+                                    </p>
+                                  </span>
+                                  <div
+                                    class="flex  justify-between items-center"
+                                    style={{ display: "flex" }}
+                                  >
+                                    <span class="">Qty : <span className="text-[#59A0B8]">{commodity.quantity}</span></span>
+                                    <span class="">
+                                      {" "}
+                                      {/* <RiDeleteBin5Line class="text-xl mx-3" /> */}
+                                    </span>
+                                  </div>
+                                </div>
+                              </div>
                               <div
-                                class="flex  justify-between items-center"
-                                style={{ display: "flex" }}
+                                class="flex hidden md:block px-2 justify-between items-center"
+                                style={{ display: "flex", marginTop: -54 }}
                               >
-                                <span class="">Qty : {commodity.quantity}</span>
-                                <span class="">
+                                <span class="hidden md:block">
+                                  Quantity :<span className="text-[#59A0B8]"> {commodity.quantity}</span>
+                                </span>
+                                <span class="hidden md:block">
                                   {" "}
                                   {/* <RiDeleteBin5Line class="text-xl mx-3" /> */}
                                 </span>
                               </div>
                             </div>
-                          </div>
-                          <div
-                            class="flex hidden md:block px-2 justify-between items-center"
-                            style={{ display: "flex", marginTop: -54 }}
-                          >
-                            <span class="hidden md:block">
-                              Quantity : {commodity.quantity}
-                            </span>
-                            <span class="hidden md:block">
-                              {" "}
-                              {/* <RiDeleteBin5Line class="text-xl mx-3" /> */}
-                            </span>
-                          </div>
-                        </div>
-                      </span>
-                      <span>
-                        <div class=" py-3 hidden md:block  text-[#59A0B8] md:pl-[6rem] md:pr-[3rem] text-[#000000] font-bold text-sm">
-                          £{(commodity.price * commodity.quantity).toFixed(2)}
-                        </div>
-                      </span>
-                    </Col>
-                  );
-                })}
+                          </span>
+                          <span>
+                            <div class="mt-4 py-3 hidden md:block  text-[#59A0B8] md:pl-[6rem] md:pr-[3rem] text-[#000000] font-bold text-sm">
+                              £{(commodity.price * commodity.quantity).toFixed(2)}
+                            </div>
+                          </span>
+                        </Col>
+                      );
+                    })}
+                  </li></ol>
               </Row>
               <hr class="mx-4 mb-4" />
             </>
